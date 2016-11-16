af7e2d1b7f12b39848a92fb954117fa8bd31bdf8
oponz <Oponz@Oponz.local>
2016-11-16 18:35:18 +0800

    1116

---
+++ 1116.js
@@ -0,0 +1,27 @@
+function d31116()
+
+{
+d3.csv("https://oponz.github.io/oponz/test222.csv", 
+
+	function(data)
+	{
+		console.log(data); 
+	}
+
+
+
+var width = 1024;
+var height = 768;
+var ctrl = d3.select(".footer").append("svg").attr("width", width).attr("height", height);
+d3.csv("./test222.csv", 
+	function(data)
+	{
+		var ln = data.length;
+		console.log(data);
+		var maxy = d3.max(data, function(d){ return d.VALUE; });
+		var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-(maxy-d.VALUE)*300});
+		ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
+	}
+
+);
+}
